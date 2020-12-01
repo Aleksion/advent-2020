04541b24132a906ee76cf190403394c3fe748452
Aleksander Rendtslev <aleks@usebounce.com>
2020-12-01 21:16:46 +0000

    feat: Implemented the find-sum-recur function

--- src/day/1/main.clj
+++ src/day/1/main.clj
@@ -3,11 +3,39 @@
              clojure.java.io :as io
              ]))
 
-(defn lines [filename]
+(def file "src/day/1/input.txt")
+
+
+(defn get-set [filename]
   (with-open [rdr (io/reader filename)]
-    (doall (line-seq rdr))))
+    (doall (set (map #(Integer/parseInt %) (line-seq rdr))))))
+
+
+
+(defn find-multiple [sum-value values]
+  (let [match
+        (first
+          (filter
+            #(contains? values (- sum-value %))
+            values))
+        ]
+    (if (= match nil)
+      nil
+      (* match  (- sum-value match)))
+    ))
 
 
 (comment
-  (lines "input.txt")
+  (find-multiple 2020 (get-set file))
+
+  (first (filter (fn [v]
+                   (let [match (find-multiple (- 2020 v) (get-set file))]
+                     (do
+                       (println v)
+                       (if (= nil match)
+                         false
+
+                         (= 2020 (+ v match)))))
+                   ) (get-set file))
+         )
   )
